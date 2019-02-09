b51cf260ba7a1342c13aec1a0e69182b18f13388
Kari Marttila <kari.marttila@tieto.com>
2019-02-09 13:05:37 +0200

    Changed demo app from openJdk10 to openJdk8 to use Alpine linux for smaller image

--- docker/resources/run-dev-mode.sh
+++ docker/resources/run-dev-mode.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 # NOTE: Uses application-dev.properties and logback-env-dev.groovy
 # See "DEV" in every log line.
-java -Dspring.profiles.active=dev --illegal-access=deny -jar ./app.jar
+java -Dspring.profiles.active=dev -jar ./app.jar
 
--- docker/resources/run-prod-mode.sh
+++ docker/resources/run-prod-mode.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 # NOTE: Uses application-prod.properties and logback-env-prod.groovy
 # See "PROD" in every log line.
-java -Dspring.profiles.active=prod --illegal-access=deny -jar ./app.jar
+java -Dspring.profiles.active=prod -jar ./app.jar
 
